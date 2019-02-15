54c6a6718b2dba4778d34c5c36352da91cc2c0d1
edison <2967899402@qq.com>
2019-02-15 20:16:58 +0800

    侧边栏完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -3,10 +3,10 @@
     <el-header class="header">
       <el-row>
         <el-col :span="4">
-          <img src="../assets/logo.png" alt="" class="logo">
+          <img src="../assets/logo.png" alt class="logo">
         </el-col>
         <el-col :span="19">
-            <h2 class="middle">电商后台管理系统</h2>
+          <h2 class="middle">电商后台管理系统</h2>
         </el-col>
         <el-col :span="1">
           <a href="#" class="logout">退出</a>
@@ -14,14 +14,84 @@
       </el-row>
     </el-header>
     <el-container>
-      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-aside class="aside" width="200px">
+        <el-menu
+          :unique-opened="true"
+          :router="true"
+          default-active="2"
+          class="el-menu-vertical-demo"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              <span>用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              <span>权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              <span>商品列表</span>
+            </el-menu-item>
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              <span>分类参数</span>
+            </el-menu-item>
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              <span>商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              <span>订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              <span>数据报表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
@@ -30,24 +100,24 @@ export default {}
   height: 100%;
 }
 .logo {
-    vertical-align: middle;
+  vertical-align: middle;
 }
 .aside {
-  background-color: skyblue;
+  background-color: #ffffff;
 }
 .main {
   background-color: orange;
 }
 .middle {
-    line-height: 60px;
-    text-align: center;
-    color: #fff;
+  line-height: 60px;
+  text-align: center;
+  color: #fff;
 }
 .logout {
-    text-decoration: none;
-    line-height: 60px;
+  text-decoration: none;
+  line-height: 60px;
 }
 .header {
-    padding: 0;
+  padding: 0;
 }
 </style>
