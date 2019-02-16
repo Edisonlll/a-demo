50a19a24c0013b55f72f7e268671d9b4581b88b1
edison <2967899402@qq.com>
2019-02-16 14:11:53 +0800

    用户列表完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -85,7 +85,9 @@
           </el-submenu>
         </el-menu>
       </el-aside>
-      <el-main class="main">Main</el-main>
+      <el-main class="main">
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
@@ -124,7 +126,7 @@ export default {
   background-color: #ffffff;
 }
 .main {
-  background-color: orange;
+  background-color: #ccc;
 }
 .middle {
   line-height: 60px;
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './assets/css/base.css'
+import moment from 'moment'
 Vue.config.productionTip = false
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
@@ -12,6 +13,9 @@ Vue.use(ElementUI)
 Vue.prototype.$http = axios
 axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
 /* eslint-disable no-new */
+Vue.filter('fmtdate', (v) => {
+ return moment(v).format('YYYY-MM-DD')
+})
 new Vue({
   el: '#app',
   router,
