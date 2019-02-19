43ef897b0b07f93c600322c0a9b2ee78f39926be
edison <2967899402@qq.com>
2019-02-19 09:46:36 +0800

    取消权限完成

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -3,29 +3,48 @@
     <cus-bread level1="权限管理" level2="角色列表"></cus-bread>
     <el-button type="primary" plain @click="showRole()" class="addrole">添加角色</el-button>
     <el-table :data="roles" style="width: 100%" height="300px">
-      <el-table-column type="expand" width="80"></el-table-column>
+      <el-table-column type="expand" width="80">
+        <template slot-scope="scope">
+          <el-row class="level1" v-for="item1 in scope.row.children" :key="item1.id">
+            <el-col :span="4">
+              <el-tag closable @close="deltag(scope.row,item1)">{{item1.authName}}</el-tag>
+              <i class="el-icon-arrow-right"></i>
+            </el-col>
+            <el-col :span="20">
+              <el-row class="level2" v-for="item2 in item1.children" :key="item2.id">
+                <el-col :span="4">
+                  <el-tag
+                    closable
+                    type="success"
+                    @close="deltag(scope.row,item2)"
+                  >{{item2.authName}}</el-tag>
+                  <i class="el-icon-arrow-right"></i>
+                </el-col>
+                <el-col :span="20">
+                  <el-tag
+                    closable
+                    type="warning"
+                    v-for="item3 in item2.children"
+                    :key="item3.id"
+                    @close="deltag(scope.row,item3)"
+                  >{{item3.authName}}</el-tag>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+          <el-row>
+            <span v-if="scope.row.children.length === 0">未分配权限</span>
+          </el-row>
+        </template>
+      </el-table-column>
       <el-table-column type="index" label="#" width="80"></el-table-column>
       <el-table-column prop="roleName" label="角色名称" width="200"></el-table-column>
       <el-table-column prop="roleDesc" label="角色描述" width="200"></el-table-column>
 
       <el-table-column label="操作">
         <template slot-scope="scope">
-          <el-button
-            type="primary"
-            icon="el-icon-edit"
-            circle
-            size="mini"
-            plain
-            @click="showDiaEditUser(scope.row)"
-          ></el-button>
-          <el-button
-            type="danger"
-            icon="el-icon-delete"
-            circle
-            size="mini"
-            plain
-            @click="showdeldia(scope.row)"
-          ></el-button>
+          <el-button type="primary" icon="el-icon-edit" circle size="mini" plain></el-button>
+          <el-button type="danger" icon="el-icon-delete" circle size="mini" plain></el-button>
           <el-button
             type="success"
             icon="el-icon-check"
@@ -47,19 +66,34 @@ export default {
       roles: []
     };
   },
-  created(){
-      this.getRolesTable()
+  created() {
+    this.getRolesTable();
   },
   methods: {
+    async deltag(role, rights) {
+      const res = await this.$http.delete(
+        `roles/${role.id}/rights/${rights.id}`
+      );
+      const {
+        data,
+        meta: { status, msg }
+      } = res.data;
+      if(status===200){
+        this.$message.success(msg)
+        role.children = data
+      }
+    },
     showRole() {},
-    async getRolesTable(){
-        const res = await this.$http.get(`roles`)
-        console.log(res);
-        const {data,meta:{status}} = res.data
-        if(status===200){
-            this.roles = data
-        }
-        
+    async getRolesTable() {
+      const res = await this.$http.get(`roles`);
+      // console.log(res);
+      const {
+        data,
+        meta: { status }
+      } = res.data;
+      if (status === 200) {
+        this.roles = data;
+      }
     }
   }
 };
@@ -71,5 +105,9 @@ export default {
 }
 .addrole {
   margin-top: 10px;
+}
+.level1,
+level2 {
+  margin-bottom: 10px;
 }
 </style>
