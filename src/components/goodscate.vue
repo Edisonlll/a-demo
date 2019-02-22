a6ae82578c896529ec8d1e819a8e1291c52f5d15
edison <2967899402@qq.com>
2019-02-22 10:28:47 +0800

    添加分类完成

--- src/components/goodscate.vue
+++ src/components/goodscate.vue
@@ -17,7 +17,7 @@
         </el-form-item>
         <!-- 级联选择器 (表单元素) -->
         <el-form-item label="分类" :label-width="formLabelWidth">
-          {{selectedOptions}}
+          <!-- {{selectedOptions}} -->
           <el-cascader
             expand-trigger="hover"
             change-on-select
@@ -116,7 +116,31 @@ export default {
   },
   methods: {
     // 添加分类 - 发送请求
-    async addCate() {},
+    async addCate() {
+      if (this.selectedOptions.length === 0) {
+        this.form.cat_pid = 0;
+        this.form.cat_level = 0;
+      }
+      if (this.selectedOptions.length === 1) {
+        this.form.cat_pid = this.selectedOptions[0];
+        this.form.cat_level = 1;
+      }
+      if (this.selectedOptions.length === 2) {
+        this.form.cat_pid = this.selectedOptions[1];
+        this.form.cat_level = 2;
+      }
+      const res = await this.$http.post(`categories`, this.form);
+      // console.log(res);
+      const {
+        meta: { msg, status }
+      } = res.data;
+      if (status === 201) {
+        this.$message.success(msg);
+        this.dialogFormVisibleAdd = false;
+        this.getGoodsCate();
+        this.form.cat_name = "";
+      }
+    },
     // 添加分类- 显示对话框
     async addGoodsCate() {
       // 获取两级分类的数据
@@ -132,7 +156,7 @@ export default {
       );
       // console.log(res)
       this.list = res.data.data.result;
-      console.log(this.list);
+      // console.log(this.list);
 
       this.total = res.data.data.total;
     },
